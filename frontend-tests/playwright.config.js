08c5a6bc79306e79cd3a487d73e2ae2eb9b6cf52
BRAHMAKESHAVA <118585883+BRAHMAKESHAVA@users.noreply.github.com>
2026-01-08 23:27:02 +0530

    Update playwright.config.js

--- frontend-tests/playwright.config.js
+++ frontend-tests/playwright.config.js
@@ -1,7 +1,18 @@
-export default defineConfig({
+const { defineConfig } = require('@playwright/test');
+
+console.log('PLAYWRIGHT CONFIG LOADED');
+
+module.exports = defineConfig({
   testDir: './tests',
+
   reporter: [['html', { open: 'never' }]],
+
+  timeout: 60000,
+
   use: {
-    headless: true
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure'
   }
 });
