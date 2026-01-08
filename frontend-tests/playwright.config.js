c12978a865b877f22fb5e8f64b4659eda83ffc3b
BRAHMAKESHAVA <118585883+BRAHMAKESHAVA@users.noreply.github.com>
2026-01-08 23:22:58 +0530

    Update playwright.config.js

--- frontend-tests/playwright.config.js
+++ frontend-tests/playwright.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from '@playwright/test';
-
-console.log('PLAYWRIGHT CONFIG LOADED');
-
 export default defineConfig({
   testDir: './tests',
-
-  reporter: [
-    ['html', { open: 'never' }],
-    ['list']
-  ],
-
-  timeout: 60000,
-
+  reporter: [['html', { open: 'never' }]],
   use: {
-    headless: true,                 //  MUST for Jenkins
-    viewport: { width: 1280, height: 720 },
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'retain-on-failure'
+    headless: true
   }
 });
