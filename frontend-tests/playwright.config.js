b4a01a22cffd65998d5c4fd69f695d8a719ec1be
BRAHMAKESHAVA <118585883+BRAHMAKESHAVA@users.noreply.github.com>
2026-01-09 23:42:55 +0530

    Update playwright.config.js

--- frontend-tests/playwright.config.js
+++ frontend-tests/playwright.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
-  use: {
-    headless: false,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
-  }
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
 });
