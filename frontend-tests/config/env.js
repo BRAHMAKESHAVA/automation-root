9c3b6b91d7b98d2bb7c0ad42b3528497fc5c9e0a
BRAHMAKESHAVA <118585883+BRAHMAKESHAVA@users.noreply.github.com>
2026-01-10 12:19:45 +0530

    Update env.js

--- frontend-tests/config/env.js
+++ frontend-tests/config/env.js
@@ -1,7 +1,1 @@
-export const env = {
-  url: 'https://soilztv43qydeftuzkqpqk2uxa0xjfvr.lambda-url.ap-south-1.on.aws/',
-  username: '9491547337',
-  password: 'Captain@7337',
 
-    loginButtonSelector: '#login'
-};
